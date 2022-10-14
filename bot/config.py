f0f3411f845b3fbc73c0c2b4bd2df6e33413cf8d
Doom <61868544+dtdoom2245@users.noreply.github.com>
2022-10-14 09:20:45 +0630

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,7 +2,7 @@ class config:
     BOT_TOKEN = "5438012911:AAH9yjd5nN9gRxkvzpHitsPbx31muLIy_d8"
     APP_ID = "6132607"
     API_HASH = "f9a39e382167a865d850da0c51790448"
-    DATABASE_URL = "postgresql://xmhrimkyuhfzwy:9437afa35ec650de8d40fe0560d6815e508b7c967731080474d85254d87e83d0@ec2-44-205-112-253.compute-1.amazonaws.com:5432/dao08ce18qd9jq"
+    DATABASE_URL = "postgresql://iczxzzslfzlmbo:8297ad9abc81f1443ae6093e234bfd0efd040caa8a53182696a59eee1ba6f95d@ec2-34-227-135-211.compute-1.amazonaws.com:5432/deorgpvem58p61"
     SUDO_USERS = "" # Sepearted by space.
     DOWNLOAD_DIRECTORY = "./downloads/"
     G_DRIVE_CLIENT_ID = "1090897738078-o0u5rkvfbq6v90sdmomcu1hsjfqli4jc.apps.googleusercontent.com"
